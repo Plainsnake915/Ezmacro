aae7cc4cf16ce4f86fbbf2c70320afd83a202bcd
Plainsnake915 <rifathc@icloud.com>
2026-08-15 18:33:03 -0400

    structure refactor

---
+++ Ezmacro/MacroEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezmacro
+{
+    public class MacroEvent
+    {
+        public string ActionType { get; set; } // e.g., "KeyPress", "MouseClick"
+        public string Detail { get; set; }     // e.g., "Space", "LeftButton"
+        public int X { get; set; }             // Mouse X coordinate
+        public int Y { get; set; }             // Mouse Y coordinate
+        public long Delay { get; set; }        // Milliseconds since last action
+    }
+}
--- Ezmacro/SettingsWindow.xaml.cs
+++ Ezmacro/SettingsWindow.xaml.cs
@@ -11,17 +11,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SharpHook.Data;
 
 namespace Ezmacro
 {
-    /// <summary>
-    /// Interaction logic for SettingsWindow.xaml
-    /// </summary>
     public partial class SettingsWindow : Window
     {
-        public SettingsWindow()
+        private MacroSettings _settings;
+
+        public SettingsWindow(MacroSettings currentSettings)
         {
             InitializeComponent();
+            _settings = currentSettings;
+
+            // Populate the dropdown boxes with all available keyboard keys
+            var allKeys = Enum.GetValues(typeof(KeyCode));
+            ComboRecordKey.ItemsSource = allKeys;
+            ComboPlayKey.ItemsSource = allKeys;
+
+            // Select the keys currently stored in our settings state
+            ComboRecordKey.SelectedItem = _settings.RecordStopKey;
+            ComboPlayKey.SelectedItem = _settings.PlaybackKey;
+            CheckBoxMouseRecording.IsChecked = _settings.MousetrackingEnabled;
+            SliderSampling.Value = _settings.Samplingrate;
+            CheckBoxShowMouse.IsChecked = !_settings.HideMouseMoves;
+            CheckBoxContinuosPlayback.IsChecked = _settings.ContinuosPlayback;
+            CheckBoxMinimize.IsChecked = _settings.AutoMinimize;
+        }
+
+        private void BtnSaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // Save choices back to the configuration object
+            _settings.RecordStopKey = (KeyCode)ComboRecordKey.SelectedItem;
+            _settings.PlaybackKey = (KeyCode)ComboPlayKey.SelectedItem;
+            _settings.MousetrackingEnabled = CheckBoxMouseRecording.IsChecked ?? true;
+            _settings.Samplingrate = (long)SliderSampling.Value;
+            _settings.HideMouseMoves = !(CheckBoxShowMouse.IsChecked ?? true);
+            _settings.ContinuosPlayback = CheckBoxContinuosPlayback.IsChecked ?? true;
+            _settings.AutoMinimize = CheckBoxMinimize.IsChecked ?? true;
+
+            _settings.Save(); // Persist the settings to disk
+
+
+
+            this.DialogResult = true; // Closes the window
+            CollectionViewSource.GetDefaultView(((MainWindow)Owner).MacroActions).Refresh(); // Refresh the DataGrid filter in the main window
         }
     }
 }
